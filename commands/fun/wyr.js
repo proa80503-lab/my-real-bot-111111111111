de467c90d690342b027541194b5344454985cd69
proa80503-lab <proa80503@gmail.com>
2026-06-21 19:46:25 +0300

    Update wyr.js

--- commands/fun/wyr.js
+++ commands/fun/wyr.js
@@ -1,23 +1,4 @@
-const { PremiumEmbedBuilder } = require('../../utils/embed-builder');
-
-module.exports = {
-    name: 'wyr',
-    aliases: ['هل_تفضل', 'تفضل'],
-    description: 'لعبة ماذا تفضل؟',
-    usage: 'wyr',
-
-    async execute(message, args) {
-        const questions = [
-            ['تفضل أن تكون غنياً', 'تفضل أن تكون مشهوراً'],
-            ['تفضل السفر للماضي', 'تفضل السفر للمستقبل'],
-            ['تفضل القهوة', 'تفضل الشاي'],
-            ['تفضل الصيف', 'تفضل الشتاء']
-        ];
-        const question = questions[Math.floor(Math.random() * questions.length)];
-        const embed = PremiumEmbedBuilder.game('🤔 هل تفضل...', null, [
-            { name: '1️⃣ الخيار الأول', value: question[0] },
-            { name: '2️⃣ الخيار الثاني', value: question[1] }
-        ]);
-        return message.reply({ embeds: [embed] });
-    }
-};
+'use strict';
+// إعادة توجيه إلى نظام الأزرار المتطور في fun-buttons.js
+const funButtons = require('./fun-buttons');
+module.exports = funButtons.wyr;
