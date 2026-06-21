837c4f0554002c2f74f0782f4496578685c0f011
proa80503-lab <proa80503@gmail.com>
2026-06-21 19:46:19 +0300

    Update roll.js

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,16 +1,4 @@
-const { PremiumEmbedBuilder } = require('../../utils/embed-builder');
-
-module.exports = {
-    name: 'roll',
-    aliases: ['نرد'],
-    description: 'رمي النرد',
-    usage: 'roll [sides]',
-
-    async execute(message, args) {
-        const sides = parseInt(args[0]) || 6;
-        if (sides < 2 || sides > 100) return message.reply('❌ عدد الأوجه بين 2 و 100!');
-        const result = Math.floor(Math.random() * sides) + 1;
-        const embed = PremiumEmbedBuilder.game('🎲 رمي النرد', `نرد بـ ${sides} أوجه: **${result}**`);
-        return message.reply({ embeds: [embed] });
-    }
-};
+'use strict';
+// إعادة توجيه إلى نظام الأزرار المتطور في fun-buttons.js
+const funButtons = require('./fun-buttons');
+module.exports = funButtons.roll;
