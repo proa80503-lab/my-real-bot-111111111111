49c31fcb6db20ae18898d07450e846fd4d2a4d76
proa80503-lab <proa80503@gmail.com>
2026-08-27 16:55:27 +0300

    Update ping.js

--- commands/main/ping.js
+++ commands/main/ping.js
@@ -15,9 +15,9 @@ let analytics = null;
 try { analytics = require('../../utils/analytics'); } catch {}
 
 module.exports = {
-    name: aliases: ['بينج', 'latency', 'اختبار', ],
+    name: 'ping', aliases: ['بينج', 'latency', 'اختبار', ],
     description: 'فحص سرعة البوت والخوادم',
-    usage: async execute(message) {
+    async execute(message) {
         const startTime = Date.now();
 
         const loadingEmbed = new EmbedBuilder()
