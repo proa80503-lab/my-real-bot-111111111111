d19adb2010b2bd8626fcc4e6a530c58c317723c0
proa80503-lab <proa80503@gmail.com>
2026-09-07 05:20:00 +0300

    Update server.js

--- src/api/server.js
+++ src/api/server.js
@@ -27,7 +27,7 @@ app.get('/health', (req, res) => {
 
 // Serve Vite frontend in production
 app.use(express.static(path.join(__dirname, '../../../dashboard-ui/dist')));
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../../../dashboard-ui/dist/index.html'));
 });
 
